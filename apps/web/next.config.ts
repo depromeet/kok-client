f7eeee636451ad234dc0037940ae5a267cab5496
shell <qkrdmstlr3@naver.com>
2025-02-24 23:58:54 +0900

    feat: vanilla-extract 추가 (#7)

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
+const withVanillaExtract = createVanillaExtractPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+const config = withVanillaExtract(nextConfig);
+
+export default config;
