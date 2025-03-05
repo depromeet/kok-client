55c6301aa0f67c7fe09ce1b15aa0683c69944a6b
hjy0951 <hjy0951@gmail.com>
2025-03-06 00:01:03 +0900

    fix: 버튼 컴포넌트 속성에 ref 추가

--- packages/ui/src/common/button/index.tsx
+++ packages/ui/src/common/button/index.tsx
@@ -1,23 +1,26 @@
-import { ButtonHTMLAttributes, PropsWithChildren } from "react";
+import { ButtonHTMLAttributes, PropsWithChildren, RefObject } from "react";
 import { buttonReceipe, ButtonVariants } from "./style.css";
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & ButtonVariants;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
+  ButtonVariants & { ref?: RefObject<HTMLButtonElement | null> };
 
 export const Button = ({
   buttonType,
   width,
   padding,
+  ref,
   children,
   ...props
 }: PropsWithChildren<ButtonProps>) => {
   return (
     <button
-      {...props}
       className={buttonReceipe({
         buttonType,
         width,
         padding,
       })}
+      ref={ref}
+      {...props}
     >
       {children}
     </button>
