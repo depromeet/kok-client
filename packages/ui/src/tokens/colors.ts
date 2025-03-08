a65ad30a37cac64f94323a38adf6cc423a69e4c9
leggo47 <113258117+thwjddlqslek@users.noreply.github.com>
2025-03-09 01:11:17 +0900

    fix: 중복 색상 코드 제거

--- packages/ui/src/tokens/colors.ts
+++ packages/ui/src/tokens/colors.ts
@@ -86,21 +86,6 @@ const scale = {
   subwayRaw8: "#D6406A",
   subwayRaw9: "#8E764B",
 
-  yellow5: "#FFFBEA",
-  yellow10: "#FFF5C6",
-  yellow20: "#FFEB87",
-  yellow30: "#FFD949",
-  yellow40: "#FFC61F",
-  yellow50: "#F4A004", // main color
-  yellow60: "#DE7C01",
-  yellow70: "#B85605",
-  yellow80: "#95420B",
-  yellow90: "#7B360C",
-  yellow95: "#471B01",
-
-  mapPolygon: "rgba(251, 83, 39, 0.12)",
-  mapMarkerBorder: "#000000",
-
   subwayAdjust1: "#006DD2",
   subwayAdjust2: "#00BF57",
   subwayAdjust3: "#F36630",
@@ -110,6 +95,9 @@ const scale = {
   subwayAdjust7: "#8F9A16",
   subwayAdjust8: "#D6406A",
   subwayAdjust9: "#8E764B",
+
+  mapPolygon: "rgba(251, 83, 39, 0.12)",
+  mapMarkerBorder: "#000000",
 };
 
 const token = {
