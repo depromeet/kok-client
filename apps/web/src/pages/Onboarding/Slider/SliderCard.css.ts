5549c296e7a3dd29e92afd21f5619e43f1562241
qkrdmstlr3 <qkrdmstlr3@naver.com>
2025-03-08 22:40:23 +0900

    feat(web): 온보딩 페이지 구현

---
+++ apps/web/src/pages/Onboarding/Slider/SliderCard.css.ts
@@ -0,0 +1,41 @@
+import { theme } from "@repo/ui/tokens";
+import { style } from "@vanilla-extract/css";
+
+export const slide = style({
+  width: "100vw",
+  maxWidth: 600,
+  height: "100%",
+  position: "relative",
+});
+
+export const titleContainer = style({
+  position: "absolute",
+  top: 65,
+  width: "100vw",
+  maxWidth: "600px",
+  display: "flex",
+  flexDirection: "column",
+  gap: 12,
+  alignItems: "center",
+});
+
+export const img = style({
+  width: "100%",
+  userSelect: "none",
+  WebkitUserSelect: "none",
+});
+
+export const title = style({
+  fontWeight: 800,
+  fontSize: 22,
+  color: theme.colors.gray95,
+});
+
+export const subtitle = style({
+  fontSize: 16,
+  fontWeight: 500,
+  whiteSpace: "pre-wrap",
+  textAlign: "center",
+  lineHeight: 1.5,
+  color: theme.colors.gray40,
+});
---
+++ apps/web/src/pages/Onboarding/Slider/useSliderDrag.ts
@@ -0,0 +1,77 @@
+import { useCallback, useRef, useState } from "react";
+import { animate, useMotionValue } from "@repo/motion";
+import { Position, useDrag } from "./useDrag";
+import { clamp } from "@repo/utils";
+
+export function useSliderDrag() {
+  const [sliderIndex, setSliderIndex] = useState(0);
+
+  const dragging = useRef(false);
+
+  const startX = useRef(0);
+  const currentX = useRef(0);
+  const moveDirection = useRef<"none" | "left" | "right">("none");
+
+  const translateXMotion = useMotionValue(0);
+
+  const onDragStart = useCallback(({ x }: Position) => {
+    if (dragging.current) {
+      return;
+    }
+
+    dragging.current = true;
+
+    startX.current = x;
+    currentX.current = x;
+  }, []);
+
+  const onDragMove = useCallback(
+    ({ x }: Position) => {
+      if (dragging.current === false) {
+        return;
+      }
+
+      const diffX = x - currentX.current;
+      const nextX = clamp(
+        translateXMotion.get() + diffX,
+        -window.innerWidth * 2,
+        0
+      );
+
+      if (startX.current - x < 20) {
+        moveDirection.current = "left";
+      } else if (startX.current - x > 20) {
+        moveDirection.current = "right";
+      }
+
+      if (nextX >= 0 || nextX <= -window.innerWidth * 2) {
+        moveDirection.current = "none";
+      }
+
+      translateXMotion.set(nextX);
+      currentX.current = x;
+    },
+    [translateXMotion]
+  );
+
+  const onDragEnd = useCallback(() => {
+    if (moveDirection.current === "left") {
+      setSliderIndex((prev) => prev - 1);
+      animate(translateXMotion, -window.innerWidth * (sliderIndex - 1));
+    } else if (moveDirection.current === "right") {
+      setSliderIndex((prev) => prev + 1);
+      animate(translateXMotion, -window.innerWidth * (sliderIndex + 1));
+    }
+
+    moveDirection.current = "none";
+    dragging.current = false;
+  }, [sliderIndex, translateXMotion]);
+
+  const dragEvents = useDrag({
+    onDragStart,
+    onDragMove,
+    onDragEnd,
+  });
+
+  return { sliderIndex, translateXMotion, dragEvents };
+}
