e0e09a283aab210eb5f874b7c1aef3a0c5989df6
qkrdmstlr3 <qkrdmstlr3@naver.com>
2025-03-09 03:29:00 +0900

    feat(ui): FixedBottomButton 컴포넌트 구현

---
+++ packages/ui/src/components/fixed-bottom-button/FixedBottomButton.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Button, ButtonProps } from "../button";
+import * as Style from "./style.css";
+import { ReactNode, useState } from "react";
+import { useVisualViewport } from "../../hook/useVisualViewport";
+import { motion } from "@repo/motion";
+
+interface Props extends ButtonProps {
+  top: ReactNode;
+}
+
+export function FixedBottomButton({ children, top, ...buttonProps }: Props) {
+  const [containerHeight, setContainerHeight] = useState(0);
+  const { offsetYMotionValue } = useVisualViewport();
+
+  return (
+    <>
+      <div style={{ height: containerHeight }} />
+      <motion.div
+        ref={(el) => {
+          if (el != null) {
+            setContainerHeight(el.clientHeight);
+          }
+        }}
+        className={Style.buttonContainer}
+        style={{ y: offsetYMotionValue }}
+      >
+        {top}
+        <Button {...buttonProps}>{children}</Button>
+      </motion.div>
+    </>
+  );
+}
---
+++ packages/ui/src/components/fixed-bottom-button/style.css.ts
@@ -0,0 +1,13 @@
+import { style } from "@vanilla-extract/css";
+
+export const buttonContainer = style({
+  width: "100%",
+  maxWidth: 600,
+  padding: "16px 20px",
+  position: "fixed",
+  bottom: 0,
+  background:
+    "linear-gradient(0deg, #FBFBFB 70.62%, rgba(251, 251, 251, 0.00) 100%)",
+  display: "flex",
+  flexDirection: "column",
+});
