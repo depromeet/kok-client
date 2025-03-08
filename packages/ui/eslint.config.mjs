40501df5c9216d9efaeb2fceeed90420dc347c41
shell <qkrdmstlr3@naver.com>
2025-03-08 15:58:35 +0900

    feat(ui): 스토리북 세팅 및 바텀싯 구현 (#19)

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,4 +1,14 @@
-import { config } from "@repo/eslint-config/react-internal";
+import { config as defaultConfig } from "@repo/eslint-config/react-internal";
 
 /** @type {import("eslint").Linter.Config} */
+const config = [
+  ...defaultConfig,
+  {
+    files: ["**/*.stories.tsx"],
+    rules: {
+      "react-hooks/rules-of-hooks": "off",
+    },
+  },
+];
+
 export default config;
