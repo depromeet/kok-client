bcfff3468ed94f4640cbfe72918e1b71e51d3343
Heojoooon <45158550+hjy0951@users.noreply.github.com>
2025-03-09 00:06:44 +0900

    design: 색상 디자인 시스템 정의 (#23)
    * design: 색상 디자인 시스템 정의
    
    * chore: 콘솔 제거
    
    * design: 색상 시스템 이름 재정의
    
    * chore: 스프레드 연산자로 색상 병합

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -1,2 +1,33 @@
 export const classMerge = (...args: (string | undefined)[]) =>
   args.filter(Boolean).join(" ");
+
+export const deepMerge = <
+  T extends Record<string, unknown>,
+  U extends Record<string, unknown>,
+>(
+  obj1: T,
+  obj2: U
+): T & U => {
+  const result = { ...obj1 } as T & U;
+
+  Object.keys(obj2).forEach((key) => {
+    const value = obj2[key];
+    if (obj2[key] && typeof obj2[key] === "object") {
+      if (result[key] && typeof result[key] === "object") {
+        // @ts-expect-error : color를 정의한 객체를 병합할 떄, 타입 지정이 어려워 선언
+        result[key] = deepMerge(
+          result[key] as Record<string, unknown>,
+          value as Record<string, unknown>
+        );
+      } else {
+        // @ts-expect-error : color를 정의한 객체를 병합할 떄, 타입 지정이 어려워 선언
+        result[key] = value;
+      }
+    } else {
+      // @ts-expect-error : color를 정의한 객체를 병합할 떄, 타입 지정이 어려워 선언
+      result[key] = value;
+    }
+  });
+
+  return result;
+};
