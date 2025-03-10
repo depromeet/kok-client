466d9e7e8c4b0d3a9fd44075d6d33fc216339535
hjy0951 <hjy0951@gmail.com>
2025-03-10 20:07:37 +0900

    design: 프로그레스바 fixed 고정 및 배경색 투명 및 white 설정 추가

--- packages/ui/src/components/progress-bar/style.css.ts
+++ packages/ui/src/components/progress-bar/style.css.ts
@@ -1,11 +1,16 @@
+import { zIndex } from "./../../../../z-index/src/index";
 import { recipe } from "@vanilla-extract/recipes";
-import { style } from "@vanilla-extract/css";
+import { style, styleVariants } from "@vanilla-extract/css";
 import { theme } from "../../tokens";
 
 export const progressBarContainerStyle = style({
   margin: "0 auto",
-  width: "168px",
+  width: "100%",
+  maxWidth: "600px",
   padding: "40px 23px",
+  position: "fixed",
+  top: 0,
+  zIndex: zIndex.floating,
 });
 
 export const progressBarRecipe = recipe({
@@ -22,5 +27,14 @@ export const progressBarRecipe = recipe({
       true: { backgroundColor: theme.colors.gray95 },
       false: { backgroundColor: theme.colors.gray15 },
     },
+  },
+});
+
+export const backgroundStyle = styleVariants({
+  transparent: {
+    backgroundColor: "transparent",
+  },
+  solid: {
+    backgroundColor: "white",
   },
 });
