3946c58d8f475decb608c4fe06aa0fe42441fe2e
leggo47 <113258117+thwjddlqslek@users.noreply.github.com>
2025-03-21 01:38:47 +0900

    feat: 결과 화면 배너 구현

--- apps/web/src/components/midpoint-result/organisms/ResultBanner.tsx
+++ apps/web/src/components/midpoint-result/organisms/ResultBanner.tsx
@@ -5,31 +5,22 @@ import { DeleteIcon } from "@repo/ui/icons";
 import { Text, Flex } from "@repo/ui/components";
 import * as styles from "./styles.css";
 import { theme } from "@repo/ui/tokens";
-// import SmallMidIcon from "@/assets/icons/SmallMidIcon";
-// import ResultPattern from "./ResultPattern";
+import ResultPattern from "./ResultPattern";
+import { getSubwayColor } from "../../../utils/subway";
 
 interface ResultBannerProps {
   onClose: () => void;
-  subway?:
-    | "line1"
-    | "line2"
-    | "line3"
-    | "line4"
-    | "line5"
-    | "line6"
-    | "line7"
-    | "line8"
-    | "line9"
-    | "default";
-  finalPlace?: string;
+  stationName?: string;
+  routes?: string[];
 }
 
 const ResultBanner = ({
   onClose,
-  subway = "line3" as const,
-  finalPlace = "디프만 모각작!",
+  stationName = "로딩중...",
+  routes = [""],
 }: ResultBannerProps) => {
   const [isBannerVisible, setIsBannerVisible] = useState(true);
+  const backgroundColor = getSubwayColor(routes[0] || null);
 
   const handleDelete = () => {
     setIsBannerVisible(false);
@@ -39,10 +30,12 @@ const ResultBanner = ({
   if (!isBannerVisible) return null;
 
   return (
-    // TODO:api 연동 이후 타입 에러 해결하기
-    <div className={styles.resultBannerContainerRecipe({ subway: subway })}>
+    <div
+      className={styles.resultBannerContainerRecipe()}
+      style={{ backgroundColor }}
+    >
       <div className={styles.patternWrapperStyle}>
-        {/* <ResultPattern /> */}
+        <ResultPattern />
       </div>
       <Flex
         direction="column"
@@ -55,9 +48,8 @@ const ResultBanner = ({
           우리의 중간 장소는?
         </Text>
         <Flex gap={4}>
-          {/* <SmallMidIcon outColor="rgba(255, 255, 255, 1)" subway={subway} /> */}
           <Text variant="heading2" style={{ color: theme.colors.gray0 }}>
-            {finalPlace}
+            {stationName}
           </Text>
         </Flex>
       </Flex>
@@ -67,4 +59,5 @@ const ResultBanner = ({
     </div>
   );
 };
+
 export default ResultBanner;
