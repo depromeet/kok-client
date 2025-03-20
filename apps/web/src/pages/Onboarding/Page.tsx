96a4632d9bb7126713e851eb5615153352169e8c
hjy0951 <hjy0951@gmail.com>
2025-03-20 17:01:33 +0900

    chore: 메인 페이지 시작하기 버튼 추가

--- apps/web/src/pages/Onboarding/Page.tsx
+++ apps/web/src/pages/Onboarding/Page.tsx
@@ -2,15 +2,21 @@
 
 import { Button } from "@repo/ui/components";
 import * as Style from "../../styles/onboard/Page.css";
-import Slider from "./Slider/Slider";
+// import Slider from "./Slider/Slider";
 import { usePressEffect } from "@repo/motion";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter();
   const { backgroundRef, containerRef, pressProps } = usePressEffect();
+
+  const handleClickStart = () => {
+    router.push("/create");
+  };
 
   return (
     <div className={Style.container}>
-      <Slider />
+      {/* <Slider /> */}
 
       <div className={Style.buttonContainer}>
         <Button
@@ -18,6 +24,7 @@ export default function Page() {
           variant="secondary"
           className={Style.button}
           {...pressProps}
+          onClick={handleClickStart}
         >
           <div ref={backgroundRef} className={Style.buttonBackground} />콕
           시작하기
