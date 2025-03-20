3a0898a60d790bd5a30a3b9c5c8f952e852a5be8
leggo47 <113258117+thwjddlqslek@users.noreply.github.com>
2025-03-21 01:41:38 +0900

    fix: 결과 화면 테스트 데이터로 검증 및 오류 수정

--- apps/web/src/components/midpoint-result/MidPointResult.tsx
+++ apps/web/src/components/midpoint-result/MidPointResult.tsx
@@ -1,27 +1,44 @@
 "use client";
 
 import { useState } from "react";
-import { useLocationCentroid } from "@/hooks/api/useLocation";
-// import { useSimpleTransfer, useComplexTransfer } from "@/hooks/api/useTransfer";
+import { useSimpleTransfer, useComplexTransfer } from "@/hooks/api/useTransfer";
+import { useRecommendStation } from "@/hooks/api/useRecommendStation";
 import MapHeader from "./organisms/MapHeader";
 import { Flex } from "@repo/ui/components";
 import { NaverMap } from "@repo/naver-map";
-// import ResultBottomSheet from "@/components/midpoint-result/organisms/ResultBottomSheet";
+import ResultBottomSheet from "@/components/midpoint-result/organisms/ResultBottomSheet";
 import { overlayStyle, mapContainer } from "./style.css";
-// import ResultBanner from "./organisms/ResultBanner";
+import ResultBanner from "./organisms/ResultBanner";
 
 const MidPointResult = () => {
   const [isOverlayVisible, setIsOverlayVisible] = useState(true);
-  const { data: centerPoint } = useLocationCentroid("ConvH");
-  // TODO: 작업 중
-  // const { data: simpleData } = useSimpleTransfer(59, {
-  //   UUID: "test_pt",
-  //   memberId: 1,
-  // });
-  // const { data: complexData } = useComplexTransfer(59, {
-  //   UUID: "test_pt",
-  //   memberId: 1,
-  // });
+  const { data: stationsResponse } = useRecommendStation("test_pt");
+
+  const stations = stationsResponse?.data;
+  const firstStation = stations?.[0]; //최종역
+  const centerPoint = {
+    latitude: firstStation?.station.latitude,
+    longitude: firstStation?.station.longitude,
+  };
+
+  const { data: simpleData } = useSimpleTransfer(
+    firstStation?.station.id ?? 59,
+    {
+      roomId: "test_pt",
+      memberId: "1",
+    }
+  );
+
+  const { data: complexData } = useComplexTransfer(
+    firstStation?.station.id ?? 59,
+    {
+      roomId: "test_pt",
+      memberId: "1",
+    }
+  );
+
+  console.log("centerPoint", centerPoint);
+  // console.log("stations", stations);
   // console.log("simpleData", simpleData);
   // console.log("complexData", complexData);
 
@@ -29,22 +46,28 @@ const MidPointResult = () => {
     <div className={mapContainer}>
       <Flex direction="column">
         <MapHeader title="중간 장소 결과" />
-        {centerPoint && (
-          <NaverMap
-            width="100vw"
-            height="100vh"
-            finaCenterMarker={centerPoint}
-          />
-        )}
+        {centerPoint.latitude !== undefined &&
+          centerPoint.longitude !== undefined && (
+            <NaverMap
+              width="100vw"
+              height="100vh"
+              finalCenterMarker={
+                centerPoint as { latitude: number; longitude: number }
+              }
+            />
+          )}
         {isOverlayVisible && <div className={overlayStyle} />}
-        {/* <ResultBanner onClose={() => setIsOverlayVisible(false)} /> */}
-        {/* TODO: 작업 중 */}
-        {/* <ResultBottomSheet
-          totalTime={simpleData?.data.totalTime}
-          transferCount={simpleData?.data.transferCount}
-          totalDistance={complexData?.data.parsedItinerary.totalDistance}
-          legs={complexData?.data.parsedItinerary.legs}
-        /> */}
+        <ResultBanner
+          onClose={() => setIsOverlayVisible(false)}
+          stationName={firstStation?.station.name}
+          routes={firstStation?.routes}
+        />
+        <ResultBottomSheet
+          totalTime={simpleData?.data?.totalTime}
+          transferCount={simpleData?.data?.transferCount}
+          totalDistance={complexData?.data?.parsedItinerary?.totalDistance}
+          legs={complexData?.data?.parsedItinerary?.legs}
+        />
       </Flex>
     </div>
   );
