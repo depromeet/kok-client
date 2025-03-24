f824675f30c834821825b39ca252c1a4e4c597df
leggo47 <113258117+thwjddlqslek@users.noreply.github.com>
2025-03-25 01:50:39 +0900

    feat: 실시간 중간 장소 화면에 카카오톡 공유 연동 (#65)
    * feat: 배너 이미지 추가
    
    * feat: 카카오 공유하기 기능 구현
    
    - 중간 지점 계산 화면에서 카카오톡 공유 기능 추가
    - 커스텀 템플릿(ID: 118630) 연동
    
    * fix: pr 리뷰 수정 반영 - KakaoTalkShareButton 재사용성 개선, 템플릿 상수 관리 추가
    
    * chore: 불필요한 파일 삭제 및 import문 제거
    
    * fix: 불필요한 Import 및 코드 제거

--- apps/web/src/components/midpoint-result/FindingMidPoint.tsx
+++ apps/web/src/components/midpoint-result/FindingMidPoint.tsx
@@ -21,10 +21,8 @@ import StartBanner from "./organisms/StartBanner";
 const FindingMidPoint = () => {
   const { data: centroid, isLoading: centroidLoading } =
     useLocationCentroid("ConvH");
-
   const { data: convH, isLoading: convHLoading } =
     useLocationConvexHull("ConvH");
-
   const markerData = convH ? convertToMarkerData(convH) : [];
   const polygonPath = convH ? convertToPolygonPath(convH) : [];
   const centerMarkerData = centroid
@@ -36,10 +34,13 @@ const FindingMidPoint = () => {
     return <div>Loading...</div>;
   }
 
+  const roomId = centerMarkerData?.roomId || "test_pt";
+  const roomName = "디프만 모각자"; // TODO: 추후 방생성 후 연동해야함
+
   return (
     <div className={mapContainer}>
       <Flex direction="column">
-        <MapHeader title="디프만 모각자" />
+        <MapHeader title={roomName} />
         <div className={refreshStyle}>
           {centerMarkerData && (
             <RefreshCenterButton
@@ -59,7 +60,11 @@ const FindingMidPoint = () => {
         />
         {isOverlayVisible && <div className={overlayStyle} />}
         <StartBanner onClose={() => setIsOverlayVisible(false)} />
-        <ParticipantBottomSheet totalParticipants={markerData.length} />
+        <ParticipantBottomSheet
+          totalParticipants={markerData.length}
+          roomId={roomId}
+          roomName={roomName}
+        />
       </Flex>
     </div>
   );
--- apps/web/src/components/midpoint-result/organisms/ParticipantBottomSheet.tsx
+++ apps/web/src/components/midpoint-result/organisms/ParticipantBottomSheet.tsx
@@ -1,23 +1,22 @@
 "use client";
-import { useRouter } from "next/navigation";
-import { Button, Flex, Text } from "@repo/ui/components";
+import { Flex, Text } from "@repo/ui/components";
 import { theme } from "@repo/ui/tokens";
 import * as styles from "./styles.css";
 import { AnimationBottomSheet } from "@repo/ui/components";
+import KakaoTalkShareButton from "@/components/share-room/molecule/KakaoTalkShareButton";
+import { KAKAO_TEMPLATE_IDS } from "@/constants/kakao-template";
 
 interface ParticipantBottomSheetProps {
   totalParticipants?: number;
+  roomId?: string;
+  roomName?: string;
 }
 
 const ParticipantBottomSheet = ({
-  totalParticipants,
+  totalParticipants = 0,
+  roomId = "",
+  roomName = "",
 }: ParticipantBottomSheetProps) => {
-  const router = useRouter();
-
-  const onClickCopyLink = () => {
-    router.push("/result");
-  };
-
   return (
     <AnimationBottomSheet
       initialY="100%"
@@ -43,9 +42,17 @@ const ParticipantBottomSheet = ({
         </Text>
       </Flex>
       <Flex as="div" direction="column" gap={20}>
-        <Button onClick={onClickCopyLink}>
-          <Text variant="title3">링크 복사하기</Text>
-        </Button>
+        <KakaoTalkShareButton
+          templateId={KAKAO_TEMPLATE_IDS.STARTING_POINT}
+          templateArgs={{
+            roomId,
+            roomName,
+            memberCount: totalParticipants,
+          }}
+          variant="primary"
+        >
+          링크 공유하기
+        </KakaoTalkShareButton>
       </Flex>
     </AnimationBottomSheet>
   );
