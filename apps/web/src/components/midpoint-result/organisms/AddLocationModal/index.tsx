f1623240b5a8e78771160c5ae1a4689addaabb18
hjy0951 <hjy0951@gmail.com>
2025-03-28 01:50:33 +0900

    design: 지하철역 더미데이터 및 검색 결과 아이템 추가

--- apps/web/src/components/midpoint-result/organisms/AddLocationModal/index.tsx
+++ apps/web/src/components/midpoint-result/organisms/AddLocationModal/index.tsx
@@ -1,14 +1,68 @@
-import { PageModal, Header, Flex, Input } from "@repo/ui/components";
+"use client";
+
+import { PageModal, Header, Flex, Input, Text } from "@repo/ui/components";
 import * as styles from "./style.css";
 import XIcon from "@/assets/icons/XIcon";
 import { SearchIcon } from "@repo/ui/icons";
+import { ChangeEvent, Fragment, useState } from "react";
+import useDebounce from "@/hooks/useDebounce";
 
 interface AddLocationModalProps {
   isOpen: boolean;
   onClose: VoidFunction;
 }
 
+const dummyList = [
+  {
+    id: 1,
+    name: "서울역",
+    lines: ["1호선", "4호선", "경의중앙선"],
+  },
+  { id: 2, name: "시청", lines: ["1호선", "2호선"] },
+  { id: 3, name: "종각", lines: ["1호선"] },
+  { id: 4, name: "을지로입구", lines: ["2호선"] },
+  { id: 5, name: "을지로3가", lines: ["2호선", "3호선"] },
+  { id: 6, name: "을지로4가", lines: ["2호선", "5호선"] },
+  { id: 7, name: "동대문", lines: ["1호선", "4호선"] },
+  { id: 8, name: "동대문역사문화공원", lines: ["2호선", "4호선", "5호선"] },
+  { id: 9, name: "충무로", lines: ["3호선", "4호선"] },
+  { id: 10, name: "명동", lines: ["4호선"] },
+  { id: 11, name: "강남", lines: ["2호선", "신분당선"] },
+  { id: 12, name: "교대", lines: ["2호선", "3호선"] },
+  { id: 13, name: "고속터미널", lines: ["3호선", "7호선", "9호선"] },
+  { id: 14, name: "삼성", lines: ["2호선"] },
+  { id: 15, name: "잠실", lines: ["2호선", "8호선"] },
+];
+
+const removeLineSuffix = (line: string): string => {
+  return line.replace(/(선|호선|철도)$/, "");
+};
+
+const getLineColorName = (lineName: string): styles.SubwayProps => {
+  if (!isNaN(Number(lineName))) return `line${lineName}` as styles.SubwayProps;
+
+  switch (lineName) {
+    case "신분당":
+      return "shinbundang";
+    // case "공항":
+    //   return "gonghang";
+    case "경의중앙":
+      return "gyeonguiJungang";
+    case "수인분당":
+      return "suinbundang";
+  }
+
+  return "default";
+};
+
 const AddLocationModal = ({ isOpen, onClose }: AddLocationModalProps) => {
+  const [inputValue, setInputValue] = useState<string>("");
+  const debouncedValue = useDebounce(inputValue, 300);
+
+  const handleChangeInputValue = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <PageModal isOpen={isOpen}>
       <div className={styles.container}>
@@ -23,6 +77,8 @@ const AddLocationModal = ({ isOpen, onClose }: AddLocationModalProps) => {
 
         <Flex as="section" direction="column" className={styles.searchSection}>
           <Input
+            value={inputValue}
+            onChange={handleChangeInputValue}
             variant="search"
             placeholder="지하철역 검색"
             rightElement={
@@ -31,6 +87,49 @@ const AddLocationModal = ({ isOpen, onClose }: AddLocationModalProps) => {
               </button>
             }
           />
+
+          <Flex as="ul" direction="column" className={styles.searchList}>
+            {dummyList
+              .filter((stationInfo) =>
+                debouncedValue.length > 0
+                  ? stationInfo.name.includes(debouncedValue)
+                  : stationInfo
+              )
+              .map((stationInfo) => (
+                <Fragment key={stationInfo.name}>
+                  <li className={styles.searchItem}>
+                    <Flex
+                      as="button"
+                      align="center"
+                      gap={8}
+                      onClick={() => console.log(stationInfo)}
+                      className={styles.searchItemButton}
+                    >
+                      <Flex gap={4} align="center">
+                        {stationInfo.lines.map(removeLineSuffix).map((line) => (
+                          <Flex
+                            key={line}
+                            direction="column"
+                            justify="center"
+                            align="center"
+                            className={styles.lineNumberRecipe({
+                              subway: getLineColorName(line),
+                            })}
+                          >
+                            <Text variant="number">{line}</Text>
+                          </Flex>
+                        ))}
+                      </Flex>
+                      <div className={styles.divider} />
+                      <Text variant="body3" className={styles.stationName}>
+                        {stationInfo.name}
+                      </Text>
+                    </Flex>
+                  </li>
+                  <div className={styles.horizontalDivider} />
+                </Fragment>
+              ))}
+          </Flex>
         </Flex>
       </div>
     </PageModal>
--- apps/web/src/components/midpoint-result/organisms/AddLocationModal/style.css.ts
+++ apps/web/src/components/midpoint-result/organisms/AddLocationModal/style.css.ts
@@ -1,4 +1,6 @@
+import { theme } from "@repo/ui/tokens";
 import { style } from "@vanilla-extract/css";
+import { recipe, RecipeVariants } from "@vanilla-extract/recipes";
 
 export const container = style({
   width: "100%",
@@ -8,4 +10,75 @@ export const container = style({
 
 export const searchSection = style({
   padding: "24px 20px 20px 20px",
+  width: "100%",
+  height: "calc(100% - 58px)",
+});
+
+export const searchList = style({
+  marginTop: "12px",
+  overflow: "auto",
+
+  scrollbarWidth: "none",
+  "::-webkit-scrollbar": {
+    display: "none",
+  },
+});
+
+export const searchItem = style({
+  margin: "24px 0",
+  color: theme.colors.text.primary,
+});
+
+export const searchItemButton = style({
+  width: "100%",
+});
+
+export const divider = style({
+  height: "14px",
+  border: `0.5px solid ${theme.colors.text.divider}`,
+});
+
+export const horizontalDivider = style({
+  width: "100%",
+  borderTop: `1px solid ${theme.colors.text.divider}`,
+});
+
+export type LineNumberVariants = RecipeVariants<typeof lineNumberRecipe>;
+export type SubwayProps = Pick<
+  NonNullable<LineNumberVariants>,
+  "subway"
+>["subway"];
+
+export const lineNumberRecipe = recipe({
+  base: {
+    padding: "2px 6px",
+    minWidth: "20px",
+    minHeight: "20px",
+    borderRadius: "100px",
+    color: "white",
+  },
+  variants: {
+    subway: {
+      line1: { background: theme.colors.subwayAdjust1 },
+      line2: { background: theme.colors.subwayAdjust2 },
+      line3: { background: theme.colors.subwayAdjust3 },
+      line4: { background: theme.colors.subwayAdjust4 },
+      line5: { background: theme.colors.subwayAdjust5 },
+      line6: { background: theme.colors.subwayAdjust6 },
+      line7: { background: theme.colors.subwayAdjust7 },
+      line8: { background: theme.colors.subwayAdjust8 },
+      line9: { background: theme.colors.subwayAdjust9 },
+      shinbundang: { background: theme.colors.subwayAdjustShinbundang },
+      gyeonguiJungang: { background: theme.colors.subwayAdjustGyeonguiJungang },
+      suinbundang: { background: theme.colors.subwayAdjustSuinbundang },
+      default: theme.colors.orange30,
+    },
+  },
+  defaultVariants: {
+    subway: "default",
+  },
+});
+
+export const stationName = style({
+  paddingTop: "1px",
 });
