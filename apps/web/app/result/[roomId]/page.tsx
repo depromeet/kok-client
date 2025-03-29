caec7dde9dc28b557f1f5f7ca9b3a054367bdc56
leggo47 <113258117+thwjddlqslek@users.noreply.github.com>
2025-03-29 15:34:15 +0900

    feat: memberId 파라미터 추가 (#88)
    * feat: memberId 파라미터 추가
    
    * fix: 빌드 에러 수정

--- apps/web/app/result/[roomId]/page.tsx
+++ apps/web/app/result/[roomId]/page.tsx
@@ -2,10 +2,13 @@ import MidPointResult from "@/components/midpoint-result/MidPointResult";
 
 export default async function FinalPage({
   params,
+  searchParams,
 }: {
   params: Promise<{ roomId: string }>;
+  searchParams?: Promise<{ memberId?: string }>;
 }) {
   const { roomId } = await params;
+  const memberId = (await searchParams)?.memberId || "";
 
-  return <MidPointResult roomId={roomId} />;
+  return <MidPointResult roomId={roomId} memberId={memberId} />;
 }
