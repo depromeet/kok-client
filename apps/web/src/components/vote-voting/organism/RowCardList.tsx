e2676f8c4e126ed0a7c42c4d9e243aa372c4f1ed
leggo47 <113258117+thwjddlqslek@users.noreply.github.com>
2025-04-04 01:56:22 +0900

    fix: 시간 변경

--- apps/web/src/components/vote-voting/organism/RowCardList.tsx
+++ apps/web/src/components/vote-voting/organism/RowCardList.tsx
@@ -4,7 +4,6 @@ import { Candidate } from "../templates/type";
 import { Spacing, Text } from "@repo/ui/components";
 import { theme } from "@repo/ui/tokens";
 import { SUBWAY_META } from "@/constants/subway";
-import { convertMinutes } from "@/components/vote-onboarding/templates/convertMinutes";
 
 interface Props {
   view: "card" | "list";
@@ -87,7 +86,7 @@ export function RowCardList({
                       이동 시간
                     </Text>
                     <Text color={theme.colors.text.secondary} variant="title3">
-                      {convertMinutes(totalTime)}
+                      {Math.floor(totalTime / 60)} 분
                     </Text>
                   </div>
                   <div className={Style.line} />
