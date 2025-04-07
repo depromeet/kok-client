5be216ab226deeb96d31b760abbca57726c32ca7
hjy0951 <hjy0951@gmail.com>
2025-04-07 17:30:45 +0900

    refactor: 마커 create & cleanUp 파일 위치 조정

--- packages/naver-map/src/overlays/marker/marker.ts
+++ packages/naver-map/src/overlays/marker/marker.ts
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRef } from "react";
-import { NaverMapInstance, NaverMapMarker } from "../../../types";
+import { NaverMapInstance, NaverMapMarker } from "../../types";
 
 interface MarkerParams {
   map: NaverMapInstance;
