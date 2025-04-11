ce3ce69ea7606694d3be95606a148c3ebe53868e
hjy0951 <hjy0951@gmail.com>
2025-04-11 18:11:26 +0900

    feat: 버스, 지하철 경로에 대해 최소 너비를 갖도록 조정

--- apps/web/src/components/midpoint-result/organisms/TransportPath.tsx
+++ apps/web/src/components/midpoint-result/organisms/TransportPath.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
 import { Flex } from "@repo/ui/components";
 import * as styles from "./styles.css";
 import TransportBar from "../molecules/TransportBar";
@@ -12,24 +12,42 @@ interface TransportPathProps {
 }
 
 const TransportPath = ({ totalTime, legs }: TransportPathProps) => {
-  const getWidthPercentage = useCallback(
-    (sectionTime: number) => {
-      if (!totalTime) return 0;
+  const calculatePercentages = useCallback(
+    (totalTime: number, infos: TransportInfo[]) => {
+      if (totalTime === 0 || !infos || infos.length === 0) return [];
 
-      const originalPercentage = (sectionTime / totalTime) * 100;
-      return originalPercentage < 5 ? 5 : Math.round(originalPercentage);
+      const rates = infos.map(
+        ({ sectionTime }) => (sectionTime / totalTime) * 100
+      );
+
+      // NOTE: 버스와 지하철의 경우, 이동 시간 표기를 위해 최소 8%의 너비를 갖도록 조정
+      const adjusted = rates.map((ratio, i) => {
+        const basePercent = ratio;
+        const mode = infos[i]!.mode;
+        const minPercent = mode === "WALK" ? 0 : 8;
+        return Math.max(basePercent, minPercent);
+      });
+
+      const totalAdjusted = adjusted.reduce((sum, val) => sum + val, 0);
+
+      return adjusted.map((val) => Math.floor((val / totalAdjusted) * 100));
     },
-    [totalTime]
+    []
+  );
+
+  const percentages = useMemo(
+    () => calculatePercentages(totalTime, legs),
+    [calculatePercentages, totalTime, legs]
   );
 
   return (
     <div className={styles.directionLineStyle}>
       <Flex direction="row" style={{ width: "100%", height: "100%" }}>
-        {legs.map((leg) => (
+        {legs.map((leg, index) => (
           <TransportBar
             key={`${leg.mode}-${leg.distance}`}
             mode={leg.mode}
-            width={getWidthPercentage(leg.sectionTime)}
+            width={percentages[index]!}
             time={leg.sectionTime}
             route={leg.route}
           />
