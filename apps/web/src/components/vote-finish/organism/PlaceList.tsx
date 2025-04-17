b51429a3d41cee4830f234593d5ec957d2c13953
leggo47 <113258117+thwjddlqslek@users.noreply.github.com>
2025-04-18 01:09:40 +0900

    feat: swiper를 통한 터치/드래그 기반 가로 스크롤 구현

--- apps/web/src/components/vote-finish/organism/PlaceList.tsx
+++ apps/web/src/components/vote-finish/organism/PlaceList.tsx
@@ -1,6 +1,9 @@
 import { Candidate } from "../templates/types";
 import { PlaceItem } from "./PlaceItem";
 import * as Style from "./PlaceList.css";
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper/modules"; // FreeMode 모듈 추가
 
 interface Props {
   placeList: Candidate[];
@@ -8,12 +11,34 @@ interface Props {
 
 export function PlaceList({ placeList }: Props) {
   return (
-    <ul className={Style.listContainer}>
-      <div style={{ marginLeft: "100px" }} />
-      {placeList.map((place) => (
-        <PlaceItem key={place.stationId} {...place} />
-      ))}
-      <div style={{ marginLeft: "100px" }} />
-    </ul>
+    <div className={Style.listContainer}>
+      <Swiper
+        modules={[FreeMode]}
+        slidesPerView="auto"
+        spaceBetween={12}
+        freeMode={true}
+        direction="horizontal"
+        slidesOffsetBefore={30}
+        style={{
+          width: "100%",
+          maxWidth: "600px",
+          paddingTop: "10.2vh",
+          height: "auto",
+        }}
+      >
+        {placeList.map((place) => (
+          <SwiperSlide
+            key={place.stationId}
+            style={{
+              width: "auto",
+              height: "auto",
+              display: "flex",
+            }}
+          >
+            <PlaceItem {...place} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 }
