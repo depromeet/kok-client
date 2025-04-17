2341bc188c835a18433efe3a39ad9059b978359c
leggo47 <113258117+thwjddlqslek@users.noreply.github.com>
2025-04-18 00:27:50 +0900

    fix: XIcon 다른 디자인으로 수정/추가

---
+++ apps/web/src/assets/icons/SmallXIcon.tsx
@@ -0,0 +1,26 @@
+const SmallXIcon = () => {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5.25391 10.5901L10.5572 5.28679"
+        stroke="white"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+      <path
+        d="M10.5586 10.5898L5.25529 5.28654"
+        stroke="white"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
+
+export default SmallXIcon;
